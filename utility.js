22973dd2b01151ed6c9db9c0211f8f8d47add757
Zenith-One <the.number.zero@gmail.com>
2013-02-25 13:40:30 -0500

    Update utility.js

--- utility.js
+++ utility.js
@@ -1,4 +1,48 @@
 /* ============================== */
+/*       Clonable Fields          */
+/* ============================== */
+
+function getAttrNum(str, pos){
+    var arr = str.split('-');
+    return arr[pos];
+}
+
+function getNewAttr(str, newNum){
+    arr = str.split('-');
+    arr[1] = newNum;
+    var out = arr.join('-');
+    return out;
+}
+
+function childRecursive(element, func){
+    func(element);
+    var children = element.children();
+    if (children.length > 0) {
+        children.each(function (){
+            childRecursive($(this), func);
+        });
+    }
+}
+
+function clearCloneValues(element){
+    if (element.attr('value') !== undefined){
+        element.val('');
+    }
+}
+
+function setCloneAttr(element, value){
+    if (element.attr('id') !== undefined){
+        element.attr('id',getNewAttr(element.attr('id'),value));
+    } else {}
+    if(element.attr('name') !== undefined){
+        element.attr('name',getNewAttr(element.attr('name'),value));
+    } else {}
+    if (element.attr('for') !== undefined){
+        element.attr('for',getNewAttr(element.attr('for'),value));
+    } else {}
+}
+
+/* ============================== */
 /*          Validation            */
 /* ============================== */
 
